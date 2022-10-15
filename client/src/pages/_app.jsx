e9b48a4f21f62ccf6cb159b73390d59d1d89f2f4
Cole Cline <me@colecline.com>
2022-10-15 17:55:03 -0500

    Created Basic Next App

---
+++ client/src/pages/_app.jsx
@@ -0,0 +1,12 @@
+import Layout from "../components/Layout";
+import "../styles/globals.css";
+
+export default function MyApp({ Component, pageProps }) {
+    return (
+        <>
+        <Layout>
+            <Component {...pageProps} />
+        </Layout>
+        </>
+    )
+}
